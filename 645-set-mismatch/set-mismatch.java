f5f0f9654f897a8cd9e55a27836f95b15ea3969a
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-05-25 15:05:36 +0530

    Time: 3 ms (77.59%) | Memory: 46 MB (29.86%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,51 +1,51 @@
 class Solution {
-    public int[] findErrorNums(int[] arr) {
-    //      int i = 0;
-    //     while (i < nums.length) {
-    //         int correct = nums[i] -1;
-    //         if (nums[i] < nums.length && nums[i] != nums[correct]) {
-    //             swap(nums, i , correct);
-    //         } else {
-    //             i++;
-    //         }
-    //     }
-        
-    //     for (int index = 0; index < nums.length; index++) {
-    //         if (nums[index] != index +1) {
-    //         return new int[] {nums[index],index+1};
-    //         }
-    //     }
-    //     return new int[] {-1,-1};
-    // }
-
-    // void swap(int[] nums, int first, int second) {
-    //     int temp = nums[first];
-    //     nums[first] = nums[second];
-    //     nums[second] = temp;
-    // }
-
-     int i = 0;
-        while (i < arr.length) {
-            int correct = arr[i] - 1;
-            if (arr[i] != arr[correct]) {
-                swap(arr, i , correct);
+    public int[] findErrorNums(int[] nums) {
+         int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] -1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i , correct);
             } else {
                 i++;
             }
         }
-
-        // search for first missing number
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index + 1) {
-                return new int[] {arr[index], index+1};
+        
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index +1) {
+            return new int[] {nums[index],index+1};
             }
         }
-        return new int[] {-1, -1};
+        return new int[] {-1,-1};
     }
 
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+    void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
+
+    //  int i = 0;
+    //     while (i < arr.length) {
+    //         int correct = arr[i] - 1;
+    //         if (arr[i] != arr[correct]) {
+    //             swap(arr, i , correct);
+    //         } else {
+    //             i++;
+    //         }
+    //     }
+
+    //     // search for first missing number
+    //     for (int index = 0; index < arr.length; index++) {
+    //         if (arr[index] != index + 1) {
+    //             return new int[] {arr[index], index+1};
+    //         }
+    //     }
+    //     return new int[] {-1, -1};
+    // }
+
+    // static void swap(int[] arr, int first, int second) {
+    //     int temp = arr[first];
+    //     arr[first] = arr[second];
+    //     arr[second] = temp;
+    // }
 }
