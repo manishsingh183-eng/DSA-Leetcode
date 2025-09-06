b7542dc62edaad82cde373bfee361dfffddaefe2
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-09-07 00:48:00 +0530

    Time: 24 ms (72.59%) | Memory: 29.2 MB (24.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -3,12 +3,9 @@ public class Solution {
         if(n==0){
             return 0;
         }
-        else if(n==1){
-            return 1;
-        }
         int prev=0;
         int curr=1;
-        for(int i=2;i<=n;i++){
+        for(int i=1;i<n;i++){
             int next=prev+curr;
             prev=curr;
             curr=next;
